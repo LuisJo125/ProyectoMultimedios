89deba2ebdc940db098cce59f949251ef12bf4cc
HarriRamirez <harrisonramirez160@gmail.com>
2024-06-06 11:34:41 -0600

    tabla citas terminada

--- apiProyecto/Cita/index.php
+++ apiProyecto/Cita/index.php
@@ -18,25 +18,25 @@
 
         case 'PUT':
             http_response_code(200);
-            actualizarCliente();
+            actualizarCita();
             break;
 
         case 'POST':            
-            insertarCliente();
+            insertarCita();
             break;
                 
         case 'DELETE':
             http_response_code(200);
-            borrarCliente();
+            borrarCita();
             break;
                     
         case 'GET':
                 if (!empty($_GET["id"])){
                     $id = intval($_GET["id"]);
-                    obtenerCliente($id);
+                    obtenerCita($id);
                 }
                 else{
-                    obtenerClientes();
+                    obtenerCitas();
                 }
             break;
                                             
@@ -52,11 +52,11 @@
     }
 
 
-    function obtenerClientes(){
+    function obtenerCitas(){
         
         global $db;
 
-            $query = "SELECT `id`, `nombre`, `estado`, `fechaactualizacion` FROM `clientes`";
+            $query = "SELECT `IdCita`, `Fecha`, `Hospital`, `IdMedico`, `IdServicio`, `IdCliente`, `Estado` FROM `EVM_Citas`";
             $stm = $db->prepare($query);
             $stm->execute();
     
@@ -66,10 +66,10 @@
         
     }
 
-    function obtenerCliente($id){
+    function obtenerCita($id){
         global $db;
 
-            $query = "SELECT `id`, `nombre`, `estado`, `fechaactualizacion` FROM `clientes` where  `id`=?";
+            $query = "SELECT `IdCita`, `Fecha`, `Hospital`, `IdMedico`, `IdServicio`, `IdCliente`, `Estado` FROM `EVM_Citas` where `id`=?";
             $stm = $db->prepare($query);            
             $stm->bindParam(1, $id);
             $stm->execute();
@@ -82,22 +82,25 @@
     }
 
 
-    function insertarCliente(){
+    function insertarCita(){
         global $db;
         $data = json_decode(file_get_contents("php://input"));
         
-        $query = "INSERT INTO `clientes` ( `nombre`, `estado`, `fechaactualizacion` ) values ( :nombre, :estado, :fechaactualizacion)";
+        $query = "INSERT INTO `EVM_Citas` ( `Fecha`, `Hospital`, `IdMedico`, `IdServicio`, `IdCliente`, `Estado` ) values ( :fecha, :hospital, :idMedico, :idServicio, :idCliente, :estado)";
         $stm = $db->prepare($query);            
-        $stm->bindParam(":nombre", $data->nombre);
-        $stm->bindParam(":estado", $data->estado);
-        $stm->bindParam(":fechaactualizacion", $data->fechaactualizacion);
+        $stm->bindParam(":fecha", $data->Fecha);
+        $stm->bindParam(":hospital", $data->Hospital);
+        $stm->bindParam(":idMedico", $data->IdMedico);
+        $stm->bindParam(":idServicio", $data->IdServicio);
+        $stm->bindParam(":idCliente", $data->IdCliente);
+        $stm->bindParam(":estado", $data->Estado);
    
         if($stm->execute()){
             
-            echo json_encode(array("message" => "Datos ingresados correct", "code" => "success"));
+            echo json_encode(array("message" => "Datos ingresados correctamente", "code" => "success"));
         }else{
             
-            echo json_encode(array("message" => "Datos ingresados incorrect", "code" => "danger"));
+            echo json_encode(array("message" => "Ocurrió un error al insertar", "code" => "danger"));
         }
 
     }
@@ -107,20 +110,22 @@
         global $db;
         $data = json_decode(file_get_contents("php://input"));
         
-        $query = "UPDATE `clientes` SET `nombre`= :nombre, `estado`=:estado, `fechaactualizacion`=:fechaactualizacion where `id`=:id";
+        $query = "UPDATE `EVM_Citas` SET `Fecha`= :fecha, `Hospital`=:hospital, `IdMedico`=:idMedico, `IdServicio`=:idServicio, `IdCliente`=:idCliente, `Estado`=:estado, where `IdCita`=:id";
           
         $stm = $db->prepare($query);            
-        $stm->bindParam(":id", $data->id);
-        $stm->bindParam(":nombre", $data->nombre);
-        $stm->bindParam(":estado", $data->estado);
-        $stm->bindParam(":fechaactualizacion", $data->fechaactualizacion);
+        $stm->bindParam(":fecha", $data->Fecha);
+        $stm->bindParam(":hospital", $data->Hospital);
+        $stm->bindParam(":idMedico", $data->IdMedico);
+        $stm->bindParam(":idServicio", $data->IdServicio);
+        $stm->bindParam(":idCliente", $data->IdCliente);
+        $stm->bindParam(":estado", $data->Estado);
    
         if($stm->execute()){
             
-            echo json_encode(array("message" => "Datos actualizados correct", "code" => "success"));
+            echo json_encode(array("message" => "Datos actualizados correctamente", "code" => "success"));
         }else{
             
-            echo json_encode(array("message" => "Datos actualizados incorrect", "code" => "danger"));
+            echo json_encode(array("message" => "Ocurrió un error al actualizar", "code" => "danger"));
         }
 
     }
@@ -130,17 +135,17 @@
         global $db;
         $data = json_decode(file_get_contents("php://input"));
         
-        $query = "DELETE FROM `clientes` where `id`=:id";
+        $query = "DELETE FROM `EVM_Citas` where `IdCita`=:id";
           
         $stm = $db->prepare($query);            
-        $stm->bindParam(":id", $data->id);
+        $stm->bindParam(":id", $data->IdCita);
    
         if($stm->execute()){
             
-            echo json_encode(array("message" => "Datos eliminados correct", "code" => "success"));
+            echo json_encode(array("message" => "Datos eliminados correctamente", "code" => "success"));
         }else{
             
-            echo json_encode(array("message" => "Datos eliminados incorrect", "code" => "danger"));
+            echo json_encode(array("message" => "Ocurrió un error al eliminar", "code" => "danger"));
         }
     }
 
